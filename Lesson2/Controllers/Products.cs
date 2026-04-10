7fb86385f07f0645a01343f160ae40998c3f158c
Артём Красов <artkrasov@gmail.com>
2026-04-10 19:16:00 +0300

    Добавил контроллер Продуктов

---
+++ Lesson2/Controllers/Products.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson2.Controllers;
+
+public class Products : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
