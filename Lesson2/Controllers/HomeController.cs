56e74e8e5cef84fd79560c2b105f5705cfa4a9d4
Артём Красов <artkrasov@gmail.com>
2026-04-10 19:13:04 +0300

    Второе занятие

---
+++ Lesson2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson2.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Details()
+    {
+        return View();
+    }
+}
